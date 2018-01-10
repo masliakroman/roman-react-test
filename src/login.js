7a2fb4017da5a2fae64f03650b5b018c8c0a9eb0
Roman <masliakroman@gamil.com>
2018-01-11 00:08:27 +0200

    add validation

--- src/login.js
+++ src/login.js
@@ -3,6 +3,12 @@ import Loader from './loader';
 import axios from 'axios';
 import update from 'react-addons-update'; // https://reactjs.org/docs/update.html
 
+const validation = {
+    email: (value) => {return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)},
+    hex: (value) => {return /^((0x){0,1}|#{0,1})([0-9A-F]{8}|[0-9A-F]{6})$/.test(value)},
+    text: (value) => {return /^[A-Za-z]+$/.test(value)}
+
+}
 
 class Login extends Component {
     constructor(props) {
@@ -15,15 +21,25 @@ class Login extends Component {
                 color: '',
                 colorName: '',
                 is_checked: false
+            },
+            formDataErrors: {
+                name: false,
+                email: false,
+                color: false,
+                colorName: false,
             }
         };
     }
 
     saveValue (e) {
         const { name, value } = e.target;
+        const validate = e.target.getAttribute('validate');
         const formData = update(this.state.formData, {[name]: {$set: value }});
+        const formDataErrors = value && validation[validate](value) ? 
+            update(this.state.formDataErrors, {[name]: {$set: false }}) :
+            update(this.state.formDataErrors, {[name]: {$set: true }});
 
-        this.setState({formData: formData});
+        this.setState({formData: formData, formDataErrors: formDataErrors});
     }
 
     saveInputValue (e) {
@@ -55,51 +71,73 @@ class Login extends Component {
     render() {
         return(
             <form className='login-form'>
-                <input
-                  type='text'
-                  className='user-name col-sm-7 row'
-                  value={this.state.formData.name}
-                  placeholder='Ваше імя'
-                  onChange={this.saveValue.bind(this)}
-                  name="name"
-                />
-                <input
-                  type='text'
-                  className='user-email col-sm-7 row'
-                  defaultValue=''
-                  placeholder='Ваш email'
-                  onChange={this.saveValue.bind(this)}
-                  name="email"
-                />
-                <input
-                  type='text'
-                  className='user-color col-sm-7 row'
-                  defaultValue=''
-                  placeholder='Color'
-                  onChange={this.saveValue.bind(this)}
-                  name="color"
-                />
-                <input
-                  type='text'
-                  className='user-color-name col-sm-7 row'
-                  defaultValue=''
-                  placeholder='Color Name'
-                  onChange={this.saveValue.bind(this)}
-                  name="colorName"
-                />
+                <div className='col-sm-7 row'>
+                    <input
+                      type='text'
+                      className='user-name'
+                      value={this.state.formData.name}
+                      placeholder='Ваше імя'
+                      onChange={(e) => this.saveValue(e)}
+                      name="name"
+                      validate="text"
+                    />
+                    <span className={this.state.formDataErrors.name ? '' : 'd-none'}>wrong name</span>
+                </div>
+                <div className='col-sm-7 row'>
+                    <input
+                      type='text'
+                      className='user-email'
+                      defaultValue=''
+                      placeholder='Ваш email'
+                      onChange={(e) => this.saveValue(e)}
+                      name="email"
+                      roman="roman"
+                      validate="email"
 
-                <input 
-                    type='checkbox' 
-                    name="is_checked"
-                    onChange={this.saveInputValue.bind(this)} defaultChecked={false} 
-                />Я согласен с правилами
+                    />
+                    <span className={this.state.formDataErrors.email ? '' : 'd-none'}>wrong name</span>
+                </div>
+                <div className='col-sm-7 row'>
+                    <input
+                      type='text'
+                      className='user-color'
+                      defaultValue=''
+                      placeholder='Color'
+                      onChange={(e) => this.saveValue(e)}
+                      name="color"
+                      validate="text"
+                    />
+                    <span className={this.state.formDataErrors.color ? '' : 'd-none'}>wrong name</span>
+                    </div>
+                <div className='col-sm-7 row'>
+                    <input
+                      type='text'
+                      className='user-color-name'
+                      defaultValue=''
+                      placeholder='Color Name'
+                      onChange={(e) => this.saveValue(e)}
+                      name="colorName"
+                      validate="hex"
+                    />
+                    <span className={this.state.formDataErrors.colorName ? '' : 'd-none'}>wrong name</span>
+                </div>
 
-                <button
-                  disabled={!this.state.formData.is_checked}
-                  className='apply-button'
-                  onClick={this.onBtnClickHandler.bind(this)}>
-                  Apply { this.state.saving ? <span>loading...</span> : null}
-                </button>
+                <div className='col-sm-7 row'>
+                    <input 
+                        type='checkbox' 
+                        name="is_checked"
+                        onChange={(e) => this.saveInputValue(e)} defaultChecked={false} 
+                    />Я согласен с правилами
+                </div>
+
+                <div className="col-sm-12">
+                    <button
+                      disabled={!this.state.formData.is_checked}
+                      className='apply-button'
+                      onClick={(e) => this.onBtnClickHandler(e)}>
+                      Apply { this.state.saving ? <span>loading...</span> : null}
+                    </button>
+                </div>
             </form>
         );
     }
